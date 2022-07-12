d5336317623b5a0a23017d2e2415b9599dea0cc4
Wilson Morales <102629672+Wilmor18@users.noreply.github.com>
2022-07-12 17:46:21 -0500

    Add files via upload

---
+++ Herencia2-main/ejecutable/ejecutable.java
@@ -0,0 +1,14 @@
+package Herencia2-main.ejecutable;
+
+public class ejecutable
+{
+    public static void main(String []args)
+    {
+
+        Poligonos miPoligonos = new Poligonos();
+
+    }
+
+
+
+}
